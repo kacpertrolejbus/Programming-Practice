ced0707655c958ba36e59ab27dc45e4d9fbb8a6b
kacpertrolejbus <kruszelnickik@gmail.com>
2026-05-28 16:51:35 +0200

    add

--- 05-ci_cd/converter.py
+++ 05-ci_cd/converter.py
@@ -4,8 +4,6 @@ def dec_to_bin(number):
     """Konwersja tu"""
     if isinstance(number, bool) or not isinstance(number, int):
         raise TypeError("Wartość naturalna")
-    
     if not 0 <= number <= 100:
         raise ValueError("Liczba nie w przedziale")
-    
     return bin(number)[2:]
