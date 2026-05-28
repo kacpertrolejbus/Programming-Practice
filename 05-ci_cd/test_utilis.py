de333a7424ce8f94d2b51d0eb5f8a904f87a1835
kacpertrolejbus <kruszelnickik@gmail.com>
2026-05-28 16:30:38 +0200

    add

--- 05-ci_cd/test_utilis.py
+++ 05-ci_cd/test_utilis.py
@@ -12,7 +12,7 @@ def test_add(a, b, expected):
     "a, b, expected", [(1, 2, -1), (2, 3, -1), (3, 4, -1), (4, 5, -1)]
 )
 def test_subtract(a, b, expected):
-    result = utils.subtract(a, b)
+    result = utilis.subtract(a, b)
     assert result == expected
 
 
@@ -20,11 +20,11 @@ def test_subtract(a, b, expected):
     "a, b, expected", [(1, 2, 2), (2, 3, 6), (3, 4, 12), (4, 5, 20)]
 )
 def test_multiply(a, b, expected):
-    result = utils.multiply(a, b)
+    result = utilis.multiply(a, b)
     assert result == expected
 
 
 @pytest.mark.parametrize("a, b, expected", [(1, 2, 0.5), (3, 4, 0.75), (4, 5, 0.8)])
 def test_divide(a, b, expected):
-    result = utils.divide(a, b)
+    result = utilis.divide(a, b)
     assert result == expected
